9aa505e10b5d14d8288e23023745a064af390e6a
HuiLi <1104349126@qq.com>
2020-05-26 21:16:30 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App'
+  // ,
+  // components: {
+   
+  // }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+
+</style>
